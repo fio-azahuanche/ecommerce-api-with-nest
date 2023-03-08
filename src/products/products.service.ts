48feec86aa1621298ab7a9c2314ec05057bf20be
Fiorela <fazahuanchef@gmail.com>
2023-03-08 18:06:33 -0500

    added search by slug and uuid

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class ProductsService {
@@ -34,11 +35,17 @@ export class ProductsService {
     });
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({id});
+  async findOne(term: string) {
+    let product: Product;
 
+    if( isUUID(term)){
+      product = await this.productRepository.findOneBy({ id: term});
+    } else {
+      product = await this.productRepository.findOneBy({ slug: term});
+    }
+  
     if(!product)
-      throw new NotFoundException(`Product with ${id} not found`);
+      throw new NotFoundException(`Product with ${term} not found`);
 
     return product;
   }
