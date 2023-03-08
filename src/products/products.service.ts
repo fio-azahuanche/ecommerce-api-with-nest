6bfdc25cd7d13c60446a42174269563dd30bb42d
Fiorela <fazahuanchef@gmail.com>
2023-03-08 16:41:08 -0500

    created handleDBExceptions

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -7,6 +7,7 @@ import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+  private readonly logger = new Logger('ProductsService');
 
   constructor(
     @InjectRepository(Product)
@@ -18,11 +19,9 @@ export class ProductsService {
       const product = this.productRepository.create(createProductDto);
       await this.productRepository.save(product);
       return product;
-      
+
     } catch (error) {
-      console.log(error);
-      throw new InternalServerErrorException("Help!")
-      
+      this.handleDBExceptions(error);
     }
   }
 
@@ -40,5 +39,13 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handleDBExceptions( error: any ){
+    if(error.code === '23505')
+      throw new BadRequestException(error.detail);
+
+    this.logger.error(error);
+    throw new InternalServerErrorException("Unexpected error, check server logs");
   }
 }
