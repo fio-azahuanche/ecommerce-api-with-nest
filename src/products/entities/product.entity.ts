f31280a9ed2e93a297876097bf193bbe871025b6
Fiorela <fazahuanchef@gmail.com>
2023-03-08 21:24:34 -0500

    added tags, ApiProperty and ApiResponse

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,41 +1,51 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Product {
+
+    @ApiProperty()
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @ApiProperty()
     @Column('text',{
         unique: true,
     })
     title: string;
 
+    @ApiProperty()
     @Column('float', {
         default: 0
     })
     price: number;
 
+    @ApiProperty()
     @Column({
         type: 'text',
         nullable: true
     })
     description: string;
 
+    @ApiProperty()
     @Column( 'text', {
         unique: true,
     })
     slug: string;
 
+    @ApiProperty()
     @Column('int', {
         default:0
     })
     stock: number;
 
+    @ApiProperty()
     @Column('text', {
         array: true
     })
     sizes: string[]
 
+    @ApiProperty()
     @Column('text')
     gender: string;
 
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,14 +1,20 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, Query } from '@nestjs/common';
-import { ProductsService } from './products.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+import { ProductsService } from './products.service';
 import { PaginationDTO } from 'src/common/dtos/pagination.dto';
 
+@ApiTags('Products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiResponse({ status: 201, description: 'Product was created', type: Product})
+  @ApiResponse({ status: 400, description: 'Bad request'})
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
