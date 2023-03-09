02f3d8a325b9fe8edbbacff11fe6d9920c008fa4
Fiorela <fazahuanchef@gmail.com>
2023-03-08 20:52:09 -0500

    added beforeUpdate

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Product {
@@ -44,6 +44,14 @@ export class Product {
         if ( !this.slug ) {
             this.slug = this.title
         }
+        this.slug = this.slug
+            .toLowerCase()
+            .replaceAll(' ', '_')
+            .replace("'",'')
+    }
+
+    @BeforeUpdate()
+    checkslugUpdate() {
         this.slug = this.slug
             .toLowerCase()
             .replaceAll(' ', '_')
