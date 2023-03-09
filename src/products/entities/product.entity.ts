b4ce999a6594dc7cd544a2a265c497968a56dae7
Fiorela <fazahuanchef@gmail.com>
2023-03-08 21:35:33 -0500

    expand ApiProperty

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -4,48 +4,75 @@ import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } fr
 @Entity()
 export class Product {
 
-    @ApiProperty()
+    @ApiProperty({
+        example: "7565c14b-35f8-458d-ab05-5a6f48c3d7bd",
+        description: 'Product ID',
+        uniqueItems: true
+    })
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: "T-shirt Caos",
+        description: 'Product Title',
+        uniqueItems: true
+    })
     @Column('text',{
         unique: true,
     })
     title: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 0,
+        description: 'Product Price',
+    })
     @Column('float', {
         default: 0
     })
     price: number;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        description: 'Product Description',
+    })
     @Column({
         type: 'text',
         nullable: true
     })
     description: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: "T_shirt_Caos",
+        description: 'Product Slug',
+        uniqueItems: true
+    })
     @Column( 'text', {
         unique: true,
     })
     slug: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 1,
+        description: 'Product Stock',
+    })
     @Column('int', {
         default:0
     })
     stock: number;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: ["S","M","L"],
+        description: 'Product Sizes',
+    })
     @Column('text', {
         array: true
     })
     sizes: string[]
 
-    @ApiProperty()
+    @ApiProperty({
+        example: "unisex",
+        description: 'Product Gender',
+    })
     @Column('text')
     gender: string;
 
