b21c0f457a57696b6bc28439617c13b10ff73342
Fiorela <fazahuanchef@gmail.com>
2023-03-08 21:13:46 -0500

    added swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +14,14 @@ async function bootstrap() {
     })
   )
 
+  const config = new DocumentBuilder()
+    .setTitle('Caos RestAPI')
+    .setDescription('Caos shop endpoints')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
