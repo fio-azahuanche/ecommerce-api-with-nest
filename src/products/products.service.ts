b9c6675b1065b99ef118435a4eb7c6a377cacaef
Fiorela <fazahuanchef@gmail.com>
2023-03-08 20:30:09 -0500

    added queryBuilder

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -41,6 +41,12 @@ export class ProductsService {
     if( isUUID(term)){
       product = await this.productRepository.findOneBy({ id: term});
     } else {
+      /* const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder
+        .where('UPPER(title)=:title or slug=:slug', {
+          title: term.toUpperCase(),
+          slug: term.toLowerCase()
+        }).getOne(); */
       product = await this.productRepository.findOneBy({ slug: term});
     }
   
