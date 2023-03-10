321b69c0b490f09fcb8d205e36018857be096b56
Fiorela <fazahuanchef@gmail.com>
2023-03-10 15:27:03 -0500

    changed synchronize for false

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { CommonModule } from './common/common.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
 
     }),
     ProductsModule,
