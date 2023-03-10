83bc79bbdfe0ea852634ecd45a2f3b00e881dc64
Fiorela <fazahuanchef@gmail.com>
2023-03-10 15:45:37 -0500

    updated app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,19 +7,20 @@ import { CommonModule } from './common/common.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+
     TypeOrmModule.forRoot({
       ssl: process.env.STAGE === 'prod',
-      /* extra: {
+      extra: {
         ssl: process.env.STAGE === 'prod' ? { rejectUnauthorized:false } : null
-      }, */
+      },
       type: 'postgres',
-      host: String(process.env.DB_HOST),
-      port: Number(process.env.DB_PORT),
-      database: String(process.env.DB_NAME),
-      username: String(process.env.DB_USERNAME),
-      password: String(process.env.DB_PASSWORD),
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: (process.env.DB_USERNAME),
+      password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
-      synchronize: false,
+      synchronize: true,
 
     }),
     ProductsModule,
